080cb65a30634c49e7c9a2d51a9ec7fec4c194da
millerm30 <millerm30@icloud.com>
2022-02-06 18:46:18 -0330

    update to js code for date

--- javascript/date.js
+++ javascript/date.js
@@ -1,47 +1,88 @@
 'use strict';
 
-let a_p = "";
-    const d = new Date();
-    const curDay = d.getDate();
-    const curMonth = d.getMonth();
-    let curMonthStr = "January";
-    if (curMonth === 1) {
-        curMonthStr = "February";
-    } else if (curMonth === 2) {
-        curMonthStr = "March";
-    } else if (curMonth === 3) {
-        curMonthStr = "April";
-    } else if (curMonth === 4) {
-        curMonthStr = "May";
-    } else if (curMonth === 5) {
-        curMonthStr = "June";
-    } else if (curMonth === 6) {
-        curMonthStr = "July";
-    } else if (curMonth === 7) {
-        curMonthStr = "August";
-    } else if (curMonth === 8) {
-        curMonthStr = "September";
-    } else if (curMonth === 9) {
-        curMonthStr = "October";
-    } else if (curMonth === 10) {
-        curMonthStr = "November";
-    } else if (curMonth === 11) {
-        curMonthStr = "December";
+let a_p = '';
+    const time = new Date();
+    const curDay = time.getDate();
+    let day;
+    switch (new Date().getDay()) {
+        case 0:
+            day = 'Sunday';
+            break;
+        case 1:
+            day = 'Monday';
+            break;
+        case 2:
+            day = 'Tuesday';
+            break;
+        case 3:
+            day = 'Wednesday';
+            break;
+        case 4:
+            day = 'Thursday';
+            break;
+        case 5:
+            day = 'Friday';
+            break;
+        default:
+            day = 'Saturday';
+            break;
     }
-    let curYear = d.getFullYear();
-    let curHour = d.getHours();
-    if (curHour < 12) {
-        a_p = "AM";
-    } else {
-        a_p = "PM";
+
+    const curMonth = time.getMonth();
+    let curMonthStr;
+    switch (curMonth) {
+        case 0:
+            curMonthStr = 'Jaunary';
+            break;
+        case 1:
+            curMonthStr = 'February';
+            break;
+        case 2:
+            curMonthStr = 'March';
+            break;
+        case 3:
+            curMonthStr = 'April';
+            break;
+        case 4:
+            curMonthStr = 'May';
+            break;
+        case 5:
+            curMonthStr = 'June';
+            break;
+        case 6:
+            curMonthStr = 'July';
+            break;
+        case 7:
+            curMonthStr = 'August';
+            break;
+        case 8:
+            curMonthStr = 'September';
+            break;
+        case 9:
+            curMonthStr = 'October';
+            break;
+        case 10:
+            curMonthStr = 'November';
+            break;
+        default:
+            curMonthStr = 'December';
+            break;
     }
+
+    const curYear = time.getFullYear();
+    let curHour = time.getHours();
     if (curHour == 0) {
         curHour = 12;
-    }
-    if (curHour > 12) {
+    } else if (curHour > 12) {
         curHour = curHour - 12;
     }
-    let curMinute = + d.getMinutes();
+    if (curHour > 12) { 
+        a_p="AM"; 
+    } else { 
+        a_p="PM"; 
+    }
+
+    let curMinute = time.getMinutes();
     curMinute = curMinute > 9 ? curMinute : '0' + curMinute;
 
-document.getElementById("today_date").innerHTML = (`${curDay} ${curMonthStr} ${curYear} <br> ${curHour}:${curMinute} ${a_p}`);
+    document.getElementById('today_date').innerHTML = (`${day} ${curMonthStr} ${curDay} ${curYear} <br> ${curHour}:${curMinute} ${a_p}`);
