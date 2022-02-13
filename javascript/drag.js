ec4d577cbe0bbcc318687bbe91108006c5456feb
millerm30 <millerm30@icloud.com>
2022-02-13 10:46:36 -0330

    setyp for drag and drop

--- javascript/drag.js
+++ javascript/drag.js
@@ -1,19 +1,56 @@
-'use strict';
+"use strict";
 
-function onDragStart(event) {
-  event.dataTransfer.setData("text", event.target.id);
-}
+const dragStart = (target) => {
+  target.classList.add("dragging");
+};
 
-function onDragOver(event) {
+const dragEnd = (target) => {
+  target.classList.remove("dragging");
+};
+
+const dragEnter = (event) => {
+  event.currentTarget.classList.add("drop");
+};
+
+const dragLeave = (event) => {
+  event.currentTarget.classList.remove("drop");
+};
+
+const drag = (event) => {
+  event.dataTransfer.setData("text/html", event.currentTarget.outerHTML);
+  event.dataTransfer.setData("text/plain", event.currentTarget.dataset.id);
+};
+
+const drop = (event) => {
+  document
+    .querySelectorAll(".column")
+    .forEach((column) => column.classList.remove("drop"));
+  document
+    .querySelector(`[data-id="${event.dataTransfer.getData("text/plain")}"]`)
+    .remove();
+
   event.preventDefault();
-}
+  event.currentTarget.innerHTML =
+    event.currentTarget.innerHTML + event.dataTransfer.getData("text/html");
+};
 
-function onDrop(event) {
-  const id = event.dataTransfer.getData("text");
+const allowDrop = (event) => {
+  event.preventDefault();
+};
 
-  const draggableElement = document.getElementById(id);
-  const dropzone = event.target;
-  dropzone.appendChild(draggableElement);
+document.querySelectorAll(".column").forEach((column) => {
+  column.addEventListener("dragenter", dragEnter);
+  column.addEventListener("dragleave", dragLeave);
+});
 
-  event.dataTransfer.clearData();
-}
+document.addEventListener("dragstart", (e) => {
+  if (e.target.className.includes("card")) {
+    dragStart(e.target);
+  }
+});
+
+document.addEventListener("dragend", (e) => {
+  if (e.target.className.includes("card")) {
+    dragEnd(e.target);
+  }
+});
