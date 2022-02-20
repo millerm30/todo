3aa11884090746954901e89c345c69b605a4c97c
millerm30 <millerm30@icloud.com>
2022-02-20 11:17:46 -0330

    changes to noteadd

--- javascript/noteAdd.js
+++ javascript/noteAdd.js
@@ -5,22 +5,39 @@
 const myForm = document.querySelector(".todo-form");
 const nameInput = document.querySelector("#note_name");
 const noteInput = document.querySelector("#textarea");
-const noteList = document.querySelector("#newNote");
+const noteList = document.querySelector(".todo-content");
 
 // OnSubmit Event Listener
 
 myForm.addEventListener("submit", onSubmit);
 
-// Functions 
+// Functions
+
+function uuidv4() {
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  );
+}
 
 function onSubmit(e) {  
     e.preventDefault();
-    const li = document.createElement("li");
-    li.appendChild(document.createTextNode(`${nameInput.value}\n${noteInput.value}`));
+    const div = document.createElement('div');
+    div.className += 'card';
+    div.classList += ` draggable="true" ondragstart="drag(event)`;
+    div.classList += ` data-id="${uuidv4()}`;
+    const h4Head = document.createElement('h4');
+    h4Head.appendChild(document.createTextNode(`${nameInput.value}`));
+    const para = document.createElement('p');
+    para.appendChild(document.createTextNode(`${noteInput.value}`));
 
-    noteList.appendChild(li);
+    noteList.appendChild(div);
+    div.appendChild(h4Head);
+    div.appendChild(para);
 
-    //clear fields
+    //clear fields after note add
     nameInput.value = "";
     noteInput.value = "";
 }
