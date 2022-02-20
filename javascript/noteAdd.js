bc5780736fa4752100a262cb1195998b1c91e5e5
millerm30 <millerm30@icloud.com>
2022-02-19 21:33:12 -0330

    changes to addnote.js

--- javascript/noteAdd.js
+++ javascript/noteAdd.js
@@ -1,17 +1,22 @@
 "use strict";
 
+// Form Reference
+
 const myForm = document.querySelector(".todo-form");
 const nameInput = document.querySelector("#note_name");
 const noteInput = document.querySelector("#textarea");
 const noteList = document.querySelector("#newNote");
 
+// OnSubmit Event Listener
 
 myForm.addEventListener("submit", onSubmit);
+
+// Functions 
 
 function onSubmit(e) {  
     e.preventDefault();
     const li = document.createElement("li");
-    li.appendChild(document.createTextNode(`${nameInput.value} ${noteInput.value}`));
+    li.appendChild(document.createTextNode(`${nameInput.value}\n${noteInput.value}`));
 
     noteList.appendChild(li);
 
