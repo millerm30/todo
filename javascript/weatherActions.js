7e220ae3ee09a6cd27795bdfd61678344469082e
millerm30 <millerm30@icloud.com>
2022-02-27 16:30:16 -0330

    added variables for weather app

--- javascript/weatherActions.js
+++ javascript/weatherActions.js
@@ -9,15 +9,3 @@ const myLocation = document.querySelector('#location');
 const weatherDesc = document.querySelector('.description');
 const tempC = document.querySelector('.c');
 const tempF = document.querySelector('.f');
-
-// Event Listener for on page load
-
-window.addEventListener('load', () => {
-    let long;
-    let lat;
-    if (navigator.geolocation)
-    navigator.geolocation.getCurrentPosition((position) => {
-        long = position.coords.longitude;
-        lat = position.coords.latitude;
-    });
-})
