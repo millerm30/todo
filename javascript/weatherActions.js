bf83585be5f980aedcba00d8a9c5487810b582a5
millerm30 <millerm30@icloud.com>
2022-03-06 14:54:42 -0330

    added weather icons

--- javascript/weatherActions.js
+++ javascript/weatherActions.js
@@ -1,11 +1,13 @@
 'use strict';
 
-// Weather API Request
-
-// Set Variables
-
-const iconImg = document.querySelector('#weather-icon');
-const myLocation = document.querySelector('#location');
-const weatherDesc = document.querySelector('.description');
-const tempC = document.querySelector('.c');
-const tempF = document.querySelector('.f');
+window.addEventListener("load", () => {
+    let long;
+    let lat;
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      document.querySelector('#location').innerHTML = `Location: ${lat} : ${long}`;
+      })
+    }
+});    
