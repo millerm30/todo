f6d5d9c2248b1704764ac3f962820c90d501c108
millerm30 <millerm30@icloud.com>
2022-03-07 12:47:02 -0330

    weatherApp function

---
+++ javascript/config.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const config = {
+  myapiToken: '774150fdf98db0b4a2d36904c6b51a62',
+};
--- javascript/weatherActions.js
+++ javascript/weatherActions.js
@@ -3,11 +3,38 @@
 window.addEventListener('load', () => {
     let long;
     let lat;
+    const token = config.myapiToken;
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
-      document.querySelector('#location').innerHTML = `Location: ${lat} : ${long}`;
+      let base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${token}&units=metric`;
+
+      axios.get(base)
+        .then((response) => {
+        let celcius = Math.round(parseFloat(response.data.main.temp));
+        let feelsLike = Math.round(parseFloat(response.data.main.feels_like));
+        let windSpeed = Math.round(parseFloat(response.data.wind.speed * 3.6));
+        let windGust = Math.round(parseFloat(response.data.wind.gust * 3.6));
+        if (isNaN(windGust)) {
+          document.querySelector('#windGust').innerHTML = 'Wind Gust: No Current Data!';
+        } else {
+          document.querySelector('#windGust').innerHTML = `Wind Gust: ${windGust} km/h`;
+        }
+        let humid = response.data.main.humidity;
+        const iconImg = document.querySelector('#weatherIcon');
+        let weatherIcon = response.data.weather[0].icon;
+        const iconUrl = `icons/${weatherIcon}.svg`;
+
+        document.querySelector('#description').innerHTML = `${response.data.weather[0].description}`;
+        document.querySelector('#temp').innerHTML = `${celcius}&deg;C`;
+        document.querySelector('#feelsLike').innerHTML = `Feels Like ${feelsLike}&deg;C`;
+        document.querySelector('#location').innerHTML = `Location: ${response.data.name}`;
+        document.querySelector('#wind').innerHTML = `Wind: ${windSpeed} km/h`;
+        document.querySelector('#humidity').innerHTML = `Humidity: ${humid}%`;
+        iconImg.src = iconUrl;
       })
-    }
+      .catch((error) => console.log('error', error));
+    })
+  }
 });    
