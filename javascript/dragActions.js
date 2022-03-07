259e0f5424e8d8df11ecf2cf886f4815078c1650
millerm30 <millerm30@icloud.com>
2022-03-07 13:36:06 -0330

    clean up drag

--- javascript/dragActions.js
+++ javascript/dragActions.js
@@ -1,56 +1,56 @@
-"use strict";
+'use strict';
 
 const dragStart = (target) => {
-  target.classList.add("dragging");
+  target.classList.add('dragging');
 };
 
 const dragEnd = (target) => {
-  target.classList.remove("dragging");
+  target.classList.remove('dragging');
 };
 
 const dragEnter = (event) => {
-  event.currentTarget.classList.add("drop");
+  event.currentTarget.classList.add('drop');
 };
 
 const dragLeave = (event) => {
-  event.currentTarget.classList.remove("drop");
+  event.currentTarget.classList.remove('drop');
 };
 
 const drag = (event) => {
-  event.dataTransfer.setData("text/html", event.currentTarget.outerHTML);
-  event.dataTransfer.setData("text/plain", event.currentTarget.dataset.id);
+  event.dataTransfer.setData('text/html', event.currentTarget.outerHTML);
+  event.dataTransfer.setData('text/plain', event.currentTarget.dataset.id);
 };
 
 const drop = (event) => {
   document
-    .querySelectorAll(".column")
-    .forEach((column) => column.classList.remove("drop"));
+    .querySelectorAll('.column')
+    .forEach((column) => column.classList.remove('drop'));
   document
-    .querySelector(`[data-id="${event.dataTransfer.getData("text/plain")}"]`)
+    .querySelector(`[data-id='${event.dataTransfer.getData('text/plain')}']`)
     .remove();
 
   event.preventDefault();
   event.currentTarget.innerHTML =
-    event.currentTarget.innerHTML + event.dataTransfer.getData("text/html");
+    event.currentTarget.innerHTML + event.dataTransfer.getData('text/html');
 };
 
 const allowDrop = (event) => {
   event.preventDefault();
 };
 
-document.querySelectorAll(".column").forEach((column) => {
-  column.addEventListener("dragenter", dragEnter);
-  column.addEventListener("dragleave", dragLeave);
+document.querySelectorAll('.column').forEach((column) => {
+  column.addEventListener('dragenter', dragEnter);
+  column.addEventListener('dragleave', dragLeave);
 });
 
-document.addEventListener("dragstart", (e) => {
-  if (e.target.className.includes("card")) {
+document.addEventListener('dragstart', (e) => {
+  if (e.target.className.includes('card')) {
     dragStart(e.target);
   }
 });
 
-document.addEventListener("dragend", (e) => {
-  if (e.target.className.includes("card")) {
+document.addEventListener('dragend', (e) => {
+  if (e.target.className.includes('card')) {
     dragEnd(e.target);
   }
 });
