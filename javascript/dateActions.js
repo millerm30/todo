51f551a9382251dce23fc3135ffa864b7b1fde72
millerm30 <millerm30@icloud.com>
2022-03-10 21:07:20 -0330

    changes

--- javascript/dateActions.js
+++ javascript/dateActions.js
@@ -4,35 +4,15 @@
 
 setInterval(myTimer, 1000);
 
-// Date Function
-
 function myTimer() {
+    const time = new Date();
+    const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const curDay = dayNames[time.getDay()];
+    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const curMonth = monthNames[time.getMonth()];
+    const curDate = time.getDate();
+    const curYear = time.getFullYear();
+    const curTime = time.toLocaleTimeString();
 
-const time = new Date();
-
-// Get Current Day of Week
-
-const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const curDay = dayNames[time.getDay()];
-
-//  Get Current Month
-
-const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-const curMonth = monthNames[time.getMonth()];
-
-// Get Current Date of Month
-
-const curDate = time.getDate();
-
-// Get Current Year
-
-const curYear = time.getFullYear();
-
-// Get Current Time
-
-const curTime = time.toLocaleTimeString();
-
-// Writing script to HTML id="today_date"
-
-document.querySelector('#today_date').innerHTML = (`${curDay} ${curMonth} ${curDate} / ${curYear}<br>${curTime}`);
+    document.querySelector('#today_date').innerHTML = (`${curDay} ${curMonth} ${curDate} / ${curYear}<br>${curTime}`);
 }
