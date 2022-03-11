3353029b5ccdec808f55e6a4162d07fd5fc72127
millerm30 <millerm30@icloud.com>
2022-03-10 21:20:23 -0330

    changes to wind gust conditional statement

--- javascript/weatherActions.js
+++ javascript/weatherActions.js
@@ -17,9 +17,9 @@ window.addEventListener('load', () => {
         let windSpeed = Math.round(parseFloat(response.data.wind.speed * 3.6));
         if (response.data.wind.gust) {
           let windGust = Math.round(parseFloat(response.data.wind.gust * 3.6));
-          document.querySelector("#windGust").innerHTML = `Wind Gust: ${windGust} km/h`;
+          document.querySelector('#windGust').innerHTML = `Wind Gust: ${windGust} km/h`;
           } else {
-          document.querySelector("#windGust").innerHTML = "Wind Gust: No Current Data!";
+          document.querySelector('#windGust').innerHTML = 'Wind Gust: No Current Data!';
         }
         let humid = response.data.main.humidity;
         const iconImg = document.querySelector('#weatherIcon');
