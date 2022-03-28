8f8fed5f726fd0223972e1499d501ff1d67db07f
millerm30 <millerm30@icloud.com>
2022-03-28 20:02:18 -0230

    added contact info

---
+++ javascript/modalcontactActions.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const modal = document.querySelector('#myModal');
+const btn = document.querySelector('#myBtn');
+const span = document.getElementsByClassName('close')[0];
+
+btn.onclick = function() {
+  modal.style.display = 'block';
+}
+
+span.onclick = function() {
+  modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+}
